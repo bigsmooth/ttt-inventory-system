acd6cf3486d65843d705d7395ff7ebfcd9d57f47
bigsmooth <kevinmronot@gmail.com>
2025-07-31 20:22:57 -0400

    🔧 Fix import error by removing supplier_upload

--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,4 @@
 from .admin import admin_dashboard
 from .manager import manager_dashboard
-from .retail import retail_inventory
+from .retail import retail_dashboard
 
-__all__ = ["admin_dashboard", "manager_dashboard", "supplier_upload", "retail_inventory"]
